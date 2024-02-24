318682697c0f0dcfa2168272006edf1287b9e562
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2024-02-24 22:06:40 +0200

    Added : React Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Users from './user/pages/users';
+import NewPlace from './places/pages/NewPlace';
 
-function App() {
+const App = () => {
   return (
-    <>
-      <h1>Let's start!</h1>
-    </>
+    <Router> 
+      {/* Set up for every route, especially our pages */}
+      <Switch>
+
+        {/* Default Path Route */}
+        <Route path="/" exact={true}>
+          <Users />
+        </Route>
+
+        {/* Create a New-Place in the database */}
+        <Route path="/places/new" exact={true}>
+          <NewPlace/>
+        </Route>
+          
+
+        {/* ALLOW A REDIRECT FOR NON-EXISTENT ROUTES */}
+        <Redirect to = "/" />
+      </Switch>
+
+
+
+
+
+    
+    </Router>   
+
+
   );
 }
 
---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewPlace = () => {
+  return (
+    <h1>
+      NEW PLACE
+    </h1>
+  )
+}
+
+export default NewPlace
