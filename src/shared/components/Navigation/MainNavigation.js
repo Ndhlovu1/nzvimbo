5fba4d0736688c24b0bded7d272fb3fd65900f75
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2024-02-26 01:58:36 +0200

    Added : Desktop Header

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import MainHeader from './MainHeader';
+import NavLinks from './NavLinks';
 import './MainNavigation.css';
+
 
 const MainNavigation = props => {
   return (
@@ -13,10 +15,10 @@ const MainNavigation = props => {
         <span />
       </button>
       <h1 className="main-navigation__title">
-        <Link to="/">Nzvimbo Dzako</Link>
+        <Link to="/">My Places</Link>
       </h1>
-      <nav>
-        ...
+      <nav className='main-navigation__header-nav'>
+        <NavLinks/>
       </nav>
     </MainHeader>
   );
