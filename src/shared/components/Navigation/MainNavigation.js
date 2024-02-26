9f101ed4224677e3a73d5b3f7e676c9604d82ec8
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2024-02-26 02:28:52 +0200

    ADDED : SideDrawer with functionality and allowed

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
+import BackDrop from '../UIElements/Backdrop'
 import './MainNavigation.css';
 
 
 const MainNavigation = props => {
+  //Utilize the state to verify whether it is open or it is closed
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false)
+
+
+    const openDrawer = () => {
+      setDrawerIsOpen(true)
+    }
+
+    const closeDrawer = () => {
+      setDrawerIsOpen(false)
+    }
+
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn ">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">My Places</Link>
-      </h1>
-      <nav className='main-navigation__header-nav'>
-        <NavLinks/>
-      </nav>
-    </MainHeader>
+    <React.Fragment>
+
+    {/* //If the drawer is open, simply close it with the backdrop */}
+    {drawerIsOpen && <BackDrop onClick={closeDrawer}/>}
+
+    { drawerIsOpen ? (
+      <SideDrawer>
+        <nav className='main-navigation__drawer-nav'>
+          <NavLinks/>
+        </nav>
+      </SideDrawer>) 
+      : null
+    } 
+
+      <MainHeader>
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">My Places</Link>
+        </h1>
+        <nav className='main-navigation__header-nav'>
+          <NavLinks/>
+        </nav>
+      </MainHeader>
+    </React.Fragment>
   );
 };
 
---
+++ src/shared/components/Navigation/SideDrawer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './SideDrawer.css'
+import ReactDOM from 'react-dom'
+
+const SideDrawer = props => {
+    const content = <aside className='side-drawer'>{props.children}</aside>
+    return ReactDOM.createPortal(content, document.getElementById('drawer-hook'))
+
+}
+
+export default SideDrawer
